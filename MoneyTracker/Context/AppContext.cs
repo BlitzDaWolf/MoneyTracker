23c2d287c0814fa8e5534db52befe107b74e6de4
BlitzDaWolf <botjes2@hotmail.com>
2024-01-22 17:17:40 +0100

    Add project files.

---
+++ MoneyTracker/Context/AppContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MoneyTracker.Models;
+
+namespace MoneyTracker.Context
+{
+    public class AppContext : DbContext
+    {
+        public AppContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<PriceActivity> Activities { get; set; }
+    }
+}
---
+++ MoneyTracker/Models/PriceActivity.cs
@@ -0,0 +1,11 @@
+﻿namespace MoneyTracker.Models
+{
+    public class PriceActivity
+    {
+        public Guid Id { get; set; }
+        public DateTime Time { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Type { get; set; }
+    }
+}
